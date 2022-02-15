4dec5eb299080b7e3a148f5b15568e7ec7091e71
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:22:19 +0200

    Add files via upload

---
+++ TwoDimensional_Array_Example1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensional_Array_Example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid = new int[6, 7];
+            for (int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 7; j++)
+                {
+                    grid[i, j] = i + j;
+                    Console.Write(grid[i, j]+", ");
+                }
+                Console.WriteLine();
+            }
+            foreach(int i in grid)
+            {
+                Console.Write("One item in the grid {0} ",i);
+            }
+        }
+    }
+}
