ae072443ef6f85156031d4a335e66d985f083447
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:03:06 +0200

    Add files via upload

---
+++ Array_Example_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] states = { "Alaska", "Arisona", "California", "Delaware","Georgia","Hawaii" };
+            foreach(string s in states)
+            {
+                System.Console.WriteLine(s);
+            }
+        }
+    }
+}
