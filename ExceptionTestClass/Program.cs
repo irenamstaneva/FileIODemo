57a279ff90b632de55de06a40a7a69d14e3835ff
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:08:09 +0200

    Add files via upload

---
+++ ExceptionTestClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTestClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            try
+            {
+                int y = 100 / x;
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine($"ArithmeticException Handler: {e}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Generic Exception Handler: {e}");
+            }
+            
+        }
+    }
+}
+
