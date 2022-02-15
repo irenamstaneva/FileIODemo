b4848f8ff65b0b596bc74e3f1bdab695cd32eced
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:10:06 +0200

    Add files via upload

---
+++ FileIOGUI/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOGUI
+{
+    internal class Person
+    {
+        public Person(string FirstName, string LastName, string URL) 
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.uRL = URL;
+        }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string uRL { get; set; }
+
+        public override string ToString()
+        {
+            return "First Name:"+firstName+", Last Name:"+lastName+", URL:"+uRL;
+        }
+    }
+}
