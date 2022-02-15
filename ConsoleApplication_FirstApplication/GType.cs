e5d2a6dceddc765840532c055092240a158493ec
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 12:16:17 +0200

    Add files via upload

---
+++ ConsoleApplication_FirstApplication/GType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_FirstApplication
+{
+    public class GType
+    {
+        public GType(char type)
+        {
+            switch (type)
+            {
+                case 'S':
+                    MyGType = Type.Spiral;
+                    break;
+                case 'E':
+                    MyGType = Type.Elliptical;
+                    break;
+                case 'I':
+                    MyGType = Type.Irregular;
+                    break;
+                case 'L':
+                    MyGType = Type.Lenticular;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public object MyGType { get; set; }
+        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+    }
+}
---
+++ ConsoleApplication_FirstApplication/Galaxy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_FirstApplication
+{
+    public class Galaxy
+    {
+       public string Name { get; set; }
+
+       public double MegaLightYears { get; set; }
+        public GType GalaxyType { get; set; }
+
+    }
+}
