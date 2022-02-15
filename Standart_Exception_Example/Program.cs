b65eec99db86e962335d7a9a43914d11de2f7628
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:19:20 +0200

    Add files via upload

---
+++ Standart_Exception_Example/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standart_Exception_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = -1;
+            try
+            {
+                Console.WriteLine("Enter a positive number");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The division 100/{num} is {100 / num}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Divided by {num} is not posible");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The entered string must be a interger value.");
+            }
+        }
+    }
+}
