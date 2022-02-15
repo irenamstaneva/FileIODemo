c0e042e300c6eb09e180207a01b8bb8915198ab3
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:26:14 +0200

    Add files via upload

---
+++ While_Loop_Examples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 30)
+            {
+                counter = counter+5;
+                Console.WriteLine("Counter = "+counter);
+            }
+            Console.WriteLine("All done with counting example.");
+        }
+    }
+}
