c7329dd9fa34c545a4d081cf8cdd8ffb8a152758
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 13:07:45 +0200

    Add files via upload

---
+++ HelloWorldWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HelloWorldWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.AutoSize = true;
+            this.btnClickThis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClickThis.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnClickThis.Location = new System.Drawing.Point(166, 78);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(128, 54);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Click This";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.btn_ClickThis);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHelloWorld.Location = new System.Drawing.Point(166, 146);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 25);
+            this.lblHelloWorld.TabIndex = 1;
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.AutoSize = true;
+            this.btnChangeColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChangeColor.Location = new System.Drawing.Point(313, 78);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(139, 54);
+            this.btnChangeColor.TabIndex = 2;
+            this.btnChangeColor.Text = "Change color";
+            this.btnChangeColor.UseVisualStyleBackColor = true;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 256);
+            this.Controls.Add(this.btnChangeColor);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnChangeColorClick;
+        private System.Windows.Forms.Button btnChangeColor;
+    }
+}
+
