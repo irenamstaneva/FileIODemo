0a215894f2d294b05c276f6eb1d1485b0a68b32f
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:11:08 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileIODemo
+namespace For_Loop_Example1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\35989\OneDrive\Документи\C# кодовете на Ирена\Demo\test.txt";
-            lines = File.ReadAllLines(filePath).ToList();
-
-            List<string> lines=new List<string>();
-            List<Person> people = new List<Person>();
-            List<string> outContents = new List<string>();
-
-            foreach (string line in lines)
+            for(int i=0;i<100; i = i + 15)
             {
-                string[] items = line.Split(',');
-                Person p = new Person(items[0], items[1], items[2]);
-                people.Add(p);
+                Console.WriteLine("i = " + i);
             }
-            
-
-            foreach(Person p in people)
+            for(int j = 100; j > 0; j = j - 15)
             {
-                Console.WriteLine(p);
-                outContents.Add(p.ToString());
+                Console.WriteLine("j = "+ j);
             }
-            string outFile= @"C:\Users\35989\OneDrive\Документи\C# кодовете на Ирена\Demo\outFile.txt";
-            File.WriteAllLines(outFile,outContents);
-            Console.ReadLine();
+            for(int k=1; k<100;k = k * 3)
+            {
+                Console.WriteLine("k = "+k);
+            }
         }
     }
 }
