eddaad559b50cc45c6cc9a082d60358e0f204c06
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:13:39 +0200

    Add files via upload

---
+++ For_Loop_Example6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_Example6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Irena Staneva";
+            int countS = 0;
+            for(int i = 0; i < name.Length; i++)
+            {
+                if(name.Substring(i, 1) == "S")
+                {
+                    countS++;
+                }
+
+            }
+            Console.WriteLine("There are "+countS+" letters S");
+        }
+    }
+}
