5fc7672821f5d25665c8c9d6d6501f96e09260dc
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:11:55 +0200

    Add files via upload

---
+++ For_Loop_Example3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_Example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] names={ "Abel", "Beatris", "Cheryl", "Dobby", 
+                "Eckbert", "Fabio", "Giovani","Halga"};
+            foreach(string name in names)
+            {
+                Console.WriteLine("One of the names is: "+name);
+            }
+        }
+    }
+}
