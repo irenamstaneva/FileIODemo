deef94e76aa15478999ba8b031f7608df16c7f73
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:25:03 +0200

    Add files via upload

---
+++ While_Loop_Example5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Example5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 10;
+            do
+            {
+                y--;
+                Console.WriteLine("The number is: " + y);
+            } while (y > 0);
+            Console.WriteLine("All done here.");
+        }
+    }
+}
