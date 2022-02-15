78c3c92e7cf80c69aeec605931bfd906435e8a1d
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:20:47 +0200

    Add files via upload

---
+++ ThrowException_Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowException_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            try
+            {
+                PrintName(name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        private static void PrintName(string std)
+        {
+            if (std == null)
+            {
+                throw new NullReferenceException("Name is null.");
+            }
+            Console.WriteLine(std);
+        }
+    }
+}
