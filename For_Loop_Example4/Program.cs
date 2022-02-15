2f8ea1f0fd472a429b670ea06cc4cba8ba615e64
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:13:10 +0200

    Add files via upload

---
+++ For_Loop_Example4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_Example4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i= 0; i < 8; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write("["+i+", "+ j+"]");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
