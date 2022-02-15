53b8b9ca1cf1e0d910413a4b156dad610ff1acc9
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:23:07 +0200

    Add files via upload

---
+++ While_Loop_Example2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 0;
+            while (secretNumber < 100)
+            {
+                secretNumber=Int32.Parse(Console.ReadLine());
+                Console.WriteLine("You enter the number : " + secretNumber +
+                    ". I will ask you to enter a number greater than 100.");
+            }
+            Console.WriteLine("All done here. Your number is greater than 100.");
+        }
+    }
+}
