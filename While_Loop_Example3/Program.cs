20222d62d94fda2352aa480514a90254e0724aba
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:23:40 +0200

    Add files via upload

---
+++ While_Loop_Example3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = "";
+            Console.WriteLine("Enter the secret message, which is the password.");
+            while (secretMessage != "asdf")
+            {
+                secretMessage = Console.ReadLine();
+                
+                if(secretMessage == "asdf")
+                {
+                    break;
+                }
+                Console.WriteLine("The secret message you have entered is not the password." +
+                    " Please try again");
+            }
+            Console.WriteLine("All have done here. " +
+                "The secret message, that you have typed is the password.");
+        }
+    }
+}
