1ca9f13b8f7b52aaa7590f3bfca08aada9228ae1
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:09:15 +0200

    Add files via upload

---
+++ FileIODemo/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    internal class Person
+    {
+        public Person(string FirstName, string LastName, string uRL)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.URL = uRL;
+        }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string URL { get; set; }
+
+        public override string ToString()
+        {
+            return "First Name: " + firstName +" , Last Name: "+lastName+ " , URL: "+URL;
+        }
+    }
+}
---
+++ FileIODemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\35989\OneDrive\Документи\C# кодовете на Ирена\Demo\test.txt";
+            lines = File.ReadAllLines(filePath).ToList();
+
+            List<string> lines=new List<string>();
+            List<Person> people = new List<Person>();
+            List<string> outContents = new List<string>();
+
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+                Person p = new Person(items[0], items[1], items[2]);
+                people.Add(p);
+            }
+            
+
+            foreach(Person p in people)
+            {
+                Console.WriteLine(p);
+                outContents.Add(p.ToString());
+            }
+            string outFile= @"C:\Users\35989\OneDrive\Документи\C# кодовете на Ирена\Demo\outFile.txt";
+            File.WriteAllLines(outFile,outContents);
+            Console.ReadLine();
+        }
+    }
+}
