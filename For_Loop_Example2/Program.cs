26111bde092dac80b92472e6bec429d4108c095e
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:11:34 +0200

    Add files via upload

---
+++ For_Loop_Example2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Abel","Beatris", "Cheryl","Dobby",
+                "Eckbert","Fabio","Giovani","Helga"};
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Name number "+i+" is "+names[i]);
+            }
+            for(int i = names.Length-1;i >= 0; i--)
+            {
+                Console.WriteLine("Name number "+i+" is "+names[i]);
+            }
+        }
+    }
+}
