23a1d1abcb9a49999cfe1409b3310f2bb903ee05
Irena Staneva <99118189+irenamstaneva@users.noreply.github.com>
2022-02-15 21:24:29 +0200

    Add files via upload

---
+++ While_Loop_Example4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Example4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            while (y < -10)
+            {
+                y--;
+                Console.WriteLine("Your number"+y+" is less than -10.");
+            }
+            Console.WriteLine("The number is greater than -10. All done here.");
+        }
+    }
+}
